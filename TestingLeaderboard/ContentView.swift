c83712561a284ccfa45a7bdd27c1eebe2f4bacbd
thaxz <thaxzt@gmail.com>
2023-07-20 16:23:14 -0300

    first commit

---
+++ TestingLeaderboard/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TestingLeaderboard
+//
+//  Created by thaxz on 20/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           Button
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestingLeaderboard/TestingLeaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingLeaderboardApp.swift
+//  TestingLeaderboard
+//
+//  Created by thaxz on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingLeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
